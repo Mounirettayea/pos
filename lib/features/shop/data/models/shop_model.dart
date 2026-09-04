94a7ecaa8e2e8b008fa69fa359a6ff4d6300ab42
Mounirettayea <76437728+Mounirettayea@users.noreply.github.com>
2026-09-04 07:16:31 -0700

    Complete shop model fields

--- lib/features/shop/data/models/shop_model.dart
+++ lib/features/shop/data/models/shop_model.dart
@@ -17,9 +17,41 @@ class ShopModel extends Shop {
   @HiveField(3)
   @override
   final String address;
+  @HiveField(4)
+  @override
+  final String addressLine1;
+  @HiveField(5)
+  @override
+  final String addressLine2;
+  @HiveField(6)
+  @override
+  final String phoneNumber;
+  @HiveField(7)
+  @override
+  final String footerText;
 
-  const ShopModel({required this.name, this.upiId = '', this.phone = '', this.address = ''})
-      : super(name: name, upiId: upiId, phone: phone, address: address);
+  const ShopModel({
+    required this.name,
+    this.upiId = '',
+    this.phone = '',
+    this.address = '',
+    this.addressLine1 = '',
+    this.addressLine2 = '',
+    this.phoneNumber = '',
+    this.footerText = '',
+  }) : super(name: name, upiId: upiId, phone: phone, address: address,
+        addressLine1: addressLine1, addressLine2: addressLine2,
+        phoneNumber: phoneNumber, footerText: footerText);
 
-  Shop toEntity() => Shop(name: name, upiId: upiId, phone: phone, address: address);
+  factory ShopModel.fromEntity(Shop shop) => ShopModel(
+    name: shop.name, upiId: shop.upiId, phone: shop.phone, address: shop.address,
+    addressLine1: shop.addressLine1, addressLine2: shop.addressLine2,
+    phoneNumber: shop.phoneNumber, footerText: shop.footerText,
+  );
+
+  Shop toEntity() => Shop(
+    name: name, upiId: upiId, phone: phone, address: address,
+    addressLine1: addressLine1, addressLine2: addressLine2,
+    phoneNumber: phoneNumber, footerText: footerText,
+  );
 }
