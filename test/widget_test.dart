850169a70329aff81d0ee82ebbda16827fd1f2b4
Mounirettayea <76437728+Mounirettayea@users.noreply.github.com>
2026-09-04 09:08:19 -0700

    Fix Flutter smoke test for current POS app

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,8 +2,8 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:maison_al_teeb_pos/main.dart';
 
 void main() {
-  testWidgets('Maison Al Teeb app smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const MaisonAlTeebApp());
-    expect(find.text('MAISON AL TEEB'), findsWidgets);
+  testWidgets('POS app smoke test', (WidgetTester tester) async {
+    await tester.pumpWidget(const MaterialApp(home: LoginPage()));
+    expect(find.text('MAISON AL TEEB POS'), findsOneWidget);
   });
 }
