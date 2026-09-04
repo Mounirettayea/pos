eec5828879b0bd35a2040ecd0b0ddfee2c3acedc
Mounirettayea <76437728+Mounirettayea@users.noreply.github.com>
2026-09-04 07:16:24 -0700

    Fix shop fields used by checkout

--- lib/features/shop/domain/entities/shop.dart
+++ lib/features/shop/domain/entities/shop.dart
@@ -5,14 +5,22 @@ class Shop extends Equatable {
   final String upiId;
   final String phone;
   final String address;
+  final String addressLine1;
+  final String addressLine2;
+  final String phoneNumber;
+  final String footerText;
 
   const Shop({
     required this.name,
     this.upiId = '',
     this.phone = '',
     this.address = '',
+    this.addressLine1 = '',
+    this.addressLine2 = '',
+    this.phoneNumber = '',
+    this.footerText = '',
   });
 
   @override
-  List<Object?> get props => [name, upiId, phone, address];
+  List<Object?> get props => [name, upiId, phone, address, addressLine1, addressLine2, phoneNumber, footerText];
 }
